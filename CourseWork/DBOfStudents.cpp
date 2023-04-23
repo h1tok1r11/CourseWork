23fd724b44dfedddf5b0c88cc44c116d7a1ba87f
Rizen3 <Rizen3@DESKTOP-UL676FD>
2023-04-23 15:45:49 +0300

    add clas Menu

--- CourseWork/DBOfStudents.cpp
+++ CourseWork/DBOfStudents.cpp
@@ -17,34 +17,38 @@ int main(){
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
     
-    Subject Sub1("math", markType::bad);
-    Subject Sub2("OIB", markType::excellent);
-    Subject Sub3("Economic", markType::good);
-    Subject Sub[5] = { Sub1, Sub2, Sub3 };
-    List<Subject> list;
+    //Subject Sub1("math", markType::bad);
+    //Subject Sub2("OIB", markType::excellent);
+    //Subject Sub3("Economic", markType::good);
+    //Subject Sub[5] = { Sub1, Sub2, Sub3 };
+    //List<Subject> list;
+    ///*List<int> lst;
+    //lst.push_back(1);
+    //lst.push_back(2);
+    //lst.push_back(3);*/
 
-    /*for (int i = 0; i < 3; i++)
-        list.push_back(Sub[i]);*/
+    //for (int i = 0; i < 3; i++)
+    //    list.push_back(Sub[i]);
 
-    /*List<Subject> list;
-    list.push_back();
-    list.push_back(3);
-    list.push_back(2);
-    list.insert(15, 1);*/
+    ///*List<Subject> list;
+    //list.push_back();
+    //list.push_back(3);
+    //list.push_back(2);
+    //list.insert(15, 1);*/
 
-    /*for (int i = 0; i < list.getSize(); i++)
-    {
-        cout << list[i].getMark() << " " << list[i].getSubjectName() << endl;
-    }
-    cout << endl << endl;*/
+    ///*for (int i = 0; i < list.getSize(); i++)
+    //{
+    //    cout << list[i].getMark() << " " << list[i].getSubjectName() << endl;
+    //}
+    //cout << endl << endl;*/
 
-    /*list.removeAt(2);
-    for (int i = 0; i < list.getSize(); i++)
-    {
-        cout << list[i] << endl;
-    }*/
+    ///*list.removeAt(2);
+    //for (int i = 0; i < list.getSize(); i++)
+    //{
+    //    cout << list[i] << endl;
+    //}*/
 
 
-
-    //listOfSessions.push_front(list);
+    //List<List<Subject>> listOfSessions;
+    //listOfSessions.push_back(list);
 }
---
+++ CourseWork/Menu.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include "windows.h"
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include <stdio.h>
+#include "ClassEditData.h"
+#include "ClassList.h"
+
+class ClassMenu
+{
+private:
+	string upTitle; //Çàãîëîâîê ìåíþ (ñâåðõó)
+	ClassList items; //Ïóíêòû ìåíþ  
+	string downTitle; //Çàãîëîâîê  ìåíþ (ñíèçó)
+public:
+
+	ClassMenu(string _upTitle) {
+		upTitle = move(_upTitle);
+		//upTitle =  _upTitle; // Òàê íå íÿäî ... äîëãî;(
+	}
+
+	ClassMenu() : ClassMenu("Ãëàâíîå ìåíþ") {
+		downTitle = "Âûáåðèòå ïóíêò èç ìåíþ (÷èñëî): ";
+	}
+
+	void addMenuItem(string _item) {
+		items.push_back(_item);
+	}
+	void draw() {
+		system("cls");
+		cout << upTitle << endl;
+		//int count = items.getCount();
+		//for (int i = 0; i < count; i++) {
+		//	cout << i << ". " << items.getItem(i) << endl;
+		//}
+		items.printItems4Menu();
+		cout << downTitle << endl;
+	}
+	int run() {
+		draw();
+		ClassEditData* cl = new ClassEditData();
+		cl->setLabel("Ââåäèòå ÷èñëî");
+		int min = 0, max = items.getCount();
+		int selectItem = cl->getData(editType::onlyDigit, min, max);
+		delete cl;
+		return selectItem;
+
+	}
+
+};
